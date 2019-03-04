b714f565facbe1328a3ef39c69523bd5ba364c78
NomadFool <nomad.fool@gmail.com>
2019-03-04 20:20:22 +0300

    Noura & Rashed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+import Loading from "./Loading";
 
 class App extends Component {
   state = {
     currentAuthor: null,
-    filteredAuthors: authors
+    filteredAuthors: [],
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  selectAuthor = async author => {
+    this.setState({ loading: true });
+    let response = await axios.get(
+      `https://the-index-api.herokuapp.com/api/authors/${author.id}/`
+    );
+    this.setState({ currentAuthor: response.data });
+    this.setState({ loading: false });
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -28,6 +37,9 @@ class App extends Component {
   };
 
   getContentView = () => {
+    if (this.state.loading) {
+      return <Loading />;
+    }
     if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
@@ -40,6 +52,15 @@ class App extends Component {
       );
     }
   };
+  async componentDidMount() {
+    let response = await axios.get(
+      "https://the-index-api.herokuapp.com/api/authors/"
+    );
+    let authors = response.data;
+    this.setState({ authors: authors });
+    this.setState({ filteredAuthors: authors });
+    this.setState({ loading: false });
+  }
 
   render() {
     return (
